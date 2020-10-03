4117dbdc899e92f3bf51840063ccfa1163d7e0dd
danielmonk <danielmonk@msn.com>
2020-10-03 18:13:01 +0100

    adding index.js for start of ToDoList

--- my-todo-app/src/index.js
+++ my-todo-app/src/index.js
@@ -4,14 +4,60 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { render } from 'react-dom'
+
+const newList = async (history) => {
+  const list = await createList(uuidv4())
+  history.push(`/?uuid=${list.uuid}`)
+}
+
+const Home = (props) => {
+  const history = useHistory()
+  const uuid = queryString.parse(props.location.search).uuid
+
+  if (uuid) return TodoList(uuid)
+  else {
+    return (
+      <div className="container">
+        <div className="section">
+          <h1>Collaborative Task Lists</h1>
+          <small>
+            Powered by <a href="https://danielmonk.io">danielmonk</a>
+          </small>
+        </div>
+        <div className="section">
+          <button
+            onClick={() => {
+              newList(history)
+            }}
+          >
+            new task list
+          </button>
+        </div>
+        <div className="section build">
+          <h3>
+            React Realtime List
+          </h3>
+        </div>
+      </div>
+    )
+  }
+}
+
+render(
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* Additional Routes go here */}
+      </Switch>
+    </Router>
+  </div>,
+  document.body
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
+
