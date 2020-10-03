368b8661e6dc9bba977845cdbf49aa1c7ad3d2d5
danielmonk <danielmonk@msn.com>
2020-10-03 18:32:43 +0100

    updating index to correct imports

--- my-todo-app/src/index.js
+++ my-todo-app/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from 'react'
+import './index.css'
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
 import { render } from 'react-dom'
+import { TodoList } from './TodoList'
+import * as serviceWorker from './serviceWorker'
+import { v4 as uuidv4 } from 'uuid'
+import { createList } from './Store'
+import queryString from 'query-string'
 
 const newList = async (history) => {
   const list = await createList(uuidv4())
@@ -22,7 +24,7 @@ const Home = (props) => {
         <div className="section">
           <h1>Collaborative Task Lists</h1>
           <small>
-            Powered by <a href="https://danielmonk.io">danielmonk</a>
+            Powered by <a href="https://supabase.io">Supabase</a>
           </small>
         </div>
         <div className="section">
@@ -36,8 +38,20 @@ const Home = (props) => {
         </div>
         <div className="section build">
           <h3>
-            React Realtime List
+            Build this yourself:
+            <br />
+            <a href="https://dev.to/awalias/howto-build-collaborative-realtime-task-lists-in-react-4k52">
+              Tutorial
+            </a>{' '}
+            | <a href="https://github.com/supabase/supabase/tree/master/examples/todo">Github</a>
           </h3>
+          <a href="https://dev.to/awalias/howto-build-collaborative-realtime-task-lists-in-react-4k52">
+            <img
+              className="build-img"
+              src="https://res.cloudinary.com/practicaldev/image/fetch/s--0Q5C-mHV--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--vlXt7rid--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://dev-to-uploads.s3.amazonaws.com/i/cjjivuwyazvady0ddhyi.png"
+              alt="learn how to build this"
+            />
+          </a>
         </div>
       </div>
     )
@@ -49,7 +63,6 @@ render(
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* Additional Routes go here */}
       </Switch>
     </Router>
   </div>,
@@ -60,4 +73,3 @@ render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
-
